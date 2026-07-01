f4c972d7b809fcbdb53c0d5e215a2626153de878
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-06-30 23:41:55 -0300

    Add product copy constants

---
+++ src/lib/productCopy.ts
@@ -0,0 +1,8 @@
+export const PRODUCT_COPY = {
+  headline: 'Guarde, compartilhe e valide documentos.',
+  subheadline: 'Documentos gratis para salvar e compartilhar. Pague apenas quando quiser validar um documento ou contrato.',
+  documentPrice: 'R$ 3,90',
+  contractPrice: 'R$ 7,90',
+  documentCrypto: '0.01 POL',
+  contractCrypto: '0.02 POL',
+};
